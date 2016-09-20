4d574917d8bace4355919109f966e1022a455ba2
Ajla Cano <Ajla Cano>
2016-09-20 11:31:20 +0200

    First draft of sign-in and sign-up buttons

--- app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/MainActivity.java
+++ app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/MainActivity.java
@@ -1,7 +1,14 @@
 package com.untitledapps.meetasweedt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signup = (Button) findViewById(R.id.button);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                goToProfileActivity();
+            }
+
+        });
+    };
+
+
+
+    private void goToProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+
     }
 }
