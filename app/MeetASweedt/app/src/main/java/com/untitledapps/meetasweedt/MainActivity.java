b99d877eef1a7ec1edddf66c275969c4e5afe62f
Ajla Cano <Ajla Cano>
2016-09-21 10:44:57 +0200

    Added relevant empty layouts and activities

--- app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/MainActivity.java
+++ app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     // list of layout ids, found in R.java, after creating layout. Add when new are created!
     final static int[] SCREENS = {
-            R.layout.activity_main, R.layout.activity_matching
+            R.layout.activity_main, R.layout.activity_matching, R.layout.activity_sign_in, R.layout.activity_sign_up
     };
 
     private int currentScreen = R.layout.activity_main;
@@ -24,25 +24,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button signup = (Button) findViewById(R.id.button);
+        Button signup, signin;
+        signup = (Button) findViewById(R.id.button);
+        signin = (Button) findViewById(R.id.button);
 
+        // signup button and clicklistener
         signup.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                goToProfileActivity();
+                goToSignUpActivity();
             }
+        });
 
+        // signin button and clicklistener
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignInActivity();
+            }
         });
     };
 
 
-
-    private void goToProfileActivity() {
-        Intent intent = new Intent(this, ProfileActivity.class);
+    //sign up activity
+    private void goToSignUpActivity() {
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
 
     }
+
+    //sign in activity
+    private void goToSignInActivity() {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+
+    }
+
+
 
     public void switchToScreen(int layoutId) {
         // show screen from arg, hide the rest
