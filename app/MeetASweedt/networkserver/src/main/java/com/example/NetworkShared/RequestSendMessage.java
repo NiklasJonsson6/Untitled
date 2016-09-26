1729f292e587ac5d400db9447cd6468dda2f6495
Daniel Hesslow <Daniel.hesslow@gmail.com>
2016-09-26 20:18:02 +0200

    forgot to add again -.-

---
+++ app/MeetASweedt/networkserver/src/main/java/com/example/NetworkShared/RequestSendMessage.java
@@ -0,0 +1,27 @@
+package com.example.NetworkShared;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by Daniel on 26/09/2016.
+ */
+public class RequestSendMessage extends Request<Response>{
+    public int from_id,to_id;
+    public String message;
+    public RequestSendMessage(int from_id, int to_id, String message)
+    {
+        super(MessageType.SendMessage);
+        this.from_id = from_id;
+        this.to_id = to_id;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestSendMessage{" +
+                "message='" + message + '\'' +
+                ", to_id=" + to_id +
+                ", from_id=" + from_id +
+                '}';
+    }
+}
