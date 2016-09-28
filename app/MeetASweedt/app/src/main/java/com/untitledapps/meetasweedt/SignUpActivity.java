3e3f13f10d121d091e2e9667807763042af42c9b
AjlaC <ajlaa_94@hotmail.com>
2016-09-28 09:59:29 +0200

    Tezt

--- app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/SignUpActivity.java
+++ app/MeetASweedt/app/src/main/java/com/untitledapps/meetasweedt/SignUpActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
 
+
+    //comment to test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
